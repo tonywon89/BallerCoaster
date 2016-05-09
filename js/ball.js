4443574687fed5d50d1bda548f52a466f3d38935
Tony Won <tonywon89@gmail.com>
2016-05-09 16:29:25 -0700

    Add more precise collision between ball and track

--- js/ball.js
+++ js/ball.js
@@ -24,7 +24,36 @@ Ball.prototype.step = function () {
 
 Ball.prototype.isCollideWith = function (otherObject) {
   if (otherObject instanceof Track) {
-    if (otherObject.containPoint({x: this.pos.x, y: this.pos.y + this.radius})) {
+    var A = { x: otherObject.point1.x, y: otherObject.point1.y };
+    var B = { x: otherObject.point2.x, y: otherObject.point2.y };
+    var C = { x: this.pos.x, y: this.pos.y }
+    var LAB = Math.sqrt(Math.pow((B.x - A.x), 2) + Math.pow((B.y - A.y), 2));
+
+    var Dx = (B.x - A.x) / LAB;
+    var Dy = (B.y - A.y) / LAB;
+
+    var t = Dx * (C.x - A.x) + Dy * (C.y - A.y);
+
+    var Ex = t * Dx + A.x;
+    var Ey = t * Dy + A.y;
+
+    var LEC = Math.sqrt(Math.pow((Ex - C.x), 2) + Math.pow((Ey - C.y), 2))
+
+
+
+    // var theta = otherObject.theta;
+    // var Cx = A.x - C.x;
+    // var Cy = A.y - C.y;
+    // var gamma = Math.atan(Cx, Cy);
+    // var beta = gamma - theta;
+    // var BC = Math.sin(beta) * Math.abs(otherObject.distance(C, A));
+
+    // console.log(BC);
+    var largerX = B.x > A.x ? B.x : A.x
+    var smallerX = B.x >= A.x ? A.x : B.x
+
+    if (LEC <= this.radius && (this.pos.x <= largerX && this.pos.x >= smallerX)) {
+      // this.collidedTrack = otherObject;
       return true
     } else {
       this.isCollided = false
