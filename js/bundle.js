1cb1898be2b6efc5256cd8b7bc15a32c582d9d03
Tony Won <tonywon89@gmail.com>
2016-05-09 21:04:17 -0700

    Initial ball-generator commit

--- js/bundle.js
+++ js/bundle.js
@@ -77,8 +77,7 @@
 	  this.canvasWidth = canvasWidth;
 	  this.canvasHeight = canvasHeight;
 	};
-	
-	
+	  
 	Main.prototype.step = function () {
 	  this.checkCollisions();
 	  this.objects.forEach(function(object) {
@@ -98,7 +97,6 @@
 	  this.objects.forEach(function(obj1) {
 	    if (!(obj1 instanceof Ball)) return
 	    main.objects.some(function(obj2) {
-	      // if (obj1 === obj2) return true;
 	
 	      if (obj1.isCollideWith(obj2)) {
 	        obj1.collideWith(obj2);
